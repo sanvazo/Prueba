934f0af0cdd9126813909019719f416ca7714a10
entornos2 <entornos2@entornos>
2021-03-16 19:15:06 +0100

    Implementado constructor

--- src/Profesor.java
+++ src/Profesor.java
@@ -6,5 +6,18 @@ public class Profesor {
 	private int telefono;
 	
 	
+	/**
+	 * @param nombre
+	 * @param dni
+	 * @param telefono
+	 */
+	public Profesor(String nombre, String dni, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.telefono = telefono;
+	}
+	
+	
 
 }
